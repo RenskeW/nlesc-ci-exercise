b8d2fe7aa6177a32972d297e286c382bbc35078f
RenskeW <renskedewit1998@gmail.com>
2023-10-04 15:25:56 +0200

    added test for add()

---
+++ example.py
@@ -0,0 +1,11 @@
+import pytest
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
+
+## Tests
+@pytest.mark.parametrize("test_input1, test_input2, expected", [(1, 2, 3), ("a", "b", "ab"), (-1, 3, 2)]) 
+def test_add(test_input1, test_input2, expected):
+    assert add(test_input1, test_input2) == expected
