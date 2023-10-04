b7403c8d94e4a9eb0efc7931f6487318941e3108
RenskeW <renskedewit1998@gmail.com>
2023-10-04 15:34:50 +0200

    added test to be failed

--- example.py
+++ example.py
@@ -9,3 +9,7 @@ def subtract(a, b):
 @pytest.mark.parametrize("test_input1, test_input2, expected", [(1, 2, 3), ("a", "b", "ab"), (-1, 3, 2)]) 
 def test_add(test_input1, test_input2, expected):
     assert add(test_input1, test_input2) == expected
+
+@pytest.mark.parametrize("test_input1, test_input2, expected", [(5, 0, 5), (4.3, 0.9, 3.4)]) 
+def test_subtract(test_input1, test_input2, expected):
+    assert subtract(test_input1, test_input2) == expected
